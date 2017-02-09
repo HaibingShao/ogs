02cbf063b3a5a83d82c6238652c34138919c6314
Haibing Shao <haibing.shao@gmail.com>
2017-02-09 15:53:32 +0100

    adding the existing BHE files into the project. The current code contains a lot of errors still. Need to be further debugged.

---
+++ ProcessLib/HeatTransportBHE/BHE/BHE_Net_ELE_Distributor.h
@@ -0,0 +1,40 @@
+/**
+* \copyright
+* Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+*            Distributed under a Modified BSD License.
+*              See accompanying file LICENSE.txt or
+*              http://www.opengeosys.org/project/license
+*
+*/
+
+#ifndef BHE_NET_ELE_DISTRIBUTOR_H
+#define BHE_NET_ELE_DISTRIBUTOR_H
+
+#include "BHE_Net_ELE_Abstract.h"
+
+namespace BHE  // namespace of borehole heat exchanger
+{
+    class BHE_Net_ELE_Distributor : public BHE_Net_ELE_Abstract 
+    {
+        public:
+            BHE_Net_ELE_Distributor(std::string & name, Eigen::VectorXd & vec_Inlet_Ratio, Eigen::VectorXd & vec_Outlet_Ratio);
+
+            double get_RHS_value(); 
+
+			double set_BC(double T_in, double current_time)
+			{
+				return 0;
+			}
+
+			double get_flowrate()
+			{
+				return 0;
+			}
+
+        private:
+
+
+    }; 
+}
+
+#endif
